d3b31048f62dcedbe343db205bc46a3dd45939be
Vongsakorn4128 <wtf4128@gmail.com>
2020-01-04 23:40:13 +0700

    test glob

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,25 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = router;
+
+
+var glob = require('glob');
+ 
+glob('*/sysfile/.xlxs', function (err, files) {
+ 
+    if (err) {
+ 
+        console.log(err);
+ 
+    } else {
+ 
+        // a list of paths to javaScript files in the current working directory
+        console.log(files);
+ 
+    }
+ 
+});
